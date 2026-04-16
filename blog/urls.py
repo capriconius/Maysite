5288c66cbe84895868acbcb306089bac315f1534
DOUGALS LEAL <StarPlatinun00023@outlook.com>
2026-04-16 12:41:54 -0400

    Implementação do blog com filtro de posts publicados e detalhe por slug

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from blog.views import PostView, PostDetailView
 
 urlpatterns = [
-    path("", PostView.as_view(), name="home"),                # lista de posts publicados
-    path("<slug:slug>/", PostDetailView.as_view(), name="post_detail"),  # detalhe do post
+    path("", PostView.as_view(), name="home"),  # lista de posts publicados
+    path("post/<slug:slug>/", PostDetailView.as_view(), name="post_detail"),  # detalhe do post
 ]
+
--- blog/views/post_view.py
+++ blog/views/post_view.py
@@ -2,9 +2,10 @@ from django.views import generic
 from blog.models import Post
 
 class PostView(generic.ListView):
+    # Filtra apenas posts com status=1 (publicado) e ordena por data de criação
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
-    context_object_name = 'posts'
+    context_object_name = 'posts'   # nome usado no template
 
 class PostDetailView(generic.DetailView):
     model = Post
